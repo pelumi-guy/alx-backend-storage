a3e1fc61ec2a96ed871c4c68fae5dd87cf6b937f
Pelumi <pelumi.olalekan.g@gmail.com>
2023-05-06 05:44:30 +0000

    00 - task 3

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,6 @@
+-- A SQL script that lists all bands with Glam rock as their main style, ranked by their longevity
+SELECT band_name, 
+IFNULL(split, (SELECT EXTRACT(YEAR FROM CURDATE()))) - formed AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC
